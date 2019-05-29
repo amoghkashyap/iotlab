2cefd1cc08c48b894b790cf2be7c403fa27d643b
athreya <shreyas.athreya_s@nokia.com>
2019-05-29 14:36:21 +0530

    changes

--- databaseoperations/write.py
+++ databaseoperations/write.py
@@ -1,17 +1,24 @@
 from cassandra.cluster import Cluster
 from cassandra.query import SimpleStatement
+import sys
 
-
+#connect to a cluster
 cluster = Cluster(['10.71.11.180'])
 session = cluster.connect()
 
-keyspace = 'workshop'
+keyspace = sys.argv[1]
+tablename = keyspace+"."+sys.argv[2]
+column1 = sys.argv[3]
+column2 = sys.argv[4]
 
-session.execute("""CREATE KEYSPACE IF NOT EXISTS %s with replication={'class':'SimpleStrategy','replication_factor':1}""" %keyspace)
+
+
+session.execute("CREATE KEYSPACE IF NOT EXISTS "+keyspace+" with replication={'class':'SimpleStrategy','replication_factor':1}")
 print("created keyspace")
 
-session.execute('CREATE TABLE IF NOT EXISTS workshop.student_entries(teamName text PRIMARY KEY, status text);')
+session.execute("CREATE TABLE IF NOT EXISTS "+tablename+"(teamName text PRIMARY KEY, status text);")
 print("created table")
 
-session.execute("INSERT INTO workshop.student_entries(teamname,status) VALUES ('second entry','success');")
+
+session.execute("INSERT INTO "+tablename+"(teamname,status) VALUES ("+"'"+column1+"'"+","+"'"+column2+"'"+");")
 print("created entry in table ")
